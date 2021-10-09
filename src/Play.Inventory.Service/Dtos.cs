e6a46bafea7ca1ce5798d542ab3f3744c3d15373
Tak Yu Chan <frankychantakyu@yahoo.com.hk>
2021-10-10 00:02:29 +1300

    init

---
+++ src/Play.Inventory.Service/Dtos.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Play.Inventory.Service.Dtos
+{
+	public record GrantItemsDto(Guid UserId, Guid CatalogItemId, int Quantity);
+	public record InventoryItemDto(Guid CatalogItemId, int Quantity, string Name, string Description, DateTimeOffset AcquiredDate);
+	public record CatalogItemDto(Guid Id, string Name, string Description);
+}
