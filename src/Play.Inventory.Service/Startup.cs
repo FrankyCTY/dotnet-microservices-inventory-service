69cb1d947ba1ac83abdf771bc9098fa1b6fae2de
Tak Yu Chan <frankychantakyu@yahoo.com.hk>
2021-10-10 02:03:04 +1300

    add retry machanism and cirvuit broker for inter communication synchrous communication between services

--- src/Play.Inventory.Service/Startup.cs
+++ src/Play.Inventory.Service/Startup.cs
@@ -10,6 +10,7 @@ using Play.Common.MongoDB;
 using Play.Inventory.Service.Clients;
 using Play.Inventory.Service.Entities;
 using Polly;
+using Polly.Timeout;
 
 namespace Play.Inventory.Service
 {
@@ -35,6 +36,11 @@ namespace Play.Inventory.Service
                                 
             })
             .ConfigurePrimaryHttpMessageHandler(serviceProvider => clientHandler)
+            .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)))) // exponential timeout
+            .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().CircuitBreakerAsync(
+                3,
+                TimeSpan.FromSeconds(15)
+            ))
             .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1)); // timeout after 1 second
 
             services.AddControllers();
